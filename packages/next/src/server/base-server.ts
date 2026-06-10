273db07599f749ecb2cd337ba98ce2d0eb8bf8df
Tim Neutkens <tim@timneutkens.nl>
2026-06-10 15:53:01 +0200

    Split server render config from request state

--- packages/next/src/server/base-server.ts
+++ packages/next/src/server/base-server.ts
@@ -6,7 +6,6 @@ import type {
 import type { MiddlewareRouteMatch } from '../shared/lib/router/utils/middleware-route-matcher'
 import type { Params } from './request/params'
 import type { NextConfig, NextConfigRuntime } from './config-shared'
-import { parseMaxPostponedStateSize } from './config-shared'
 import type {
   NextParsedUrlQuery,
   NextUrlWithParsedQuery,
@@ -55,7 +54,6 @@ import { format as formatUrl } from 'url'
 import { formatHostname } from './lib/format-hostname'
 import { isRSCRequestHeader } from './lib/is-rsc-request'
 import {
-  NEXT_BUILTIN_DOCUMENT,
   STATIC_STATUS_PAGES,
   UNDERSCORE_NOT_FOUND_ROUTE,
   UNDERSCORE_NOT_FOUND_ROUTE_ENTRY,
@@ -63,7 +61,6 @@ import {
 import { getSortedRoutes, isDynamicRoute } from '../shared/lib/router/utils'
 import { execOnce } from '../shared/lib/utils'
 import { isBlockedPage } from './utils'
-import { getBotType, isBot } from '../shared/lib/router/utils/is-bot'
 import RenderResult from './render-result'
 import { removeTrailingSlash } from '../shared/lib/router/utils/remove-trailing-slash'
 import { denormalizePagePath } from '../shared/lib/page-path/denormalize-page-path'
@@ -146,7 +143,6 @@ import { NextRequestHint } from './web/adapter'
 import type { RouteModule } from './route-modules/route-module'
 import { type FallbackMode, parseFallbackField } from '../lib/fallback'
 import { SegmentPrefixRSCPathnameNormalizer } from './normalizers/request/segment-prefix-rsc'
-import { shouldServeStreamingMetadata } from './lib/streaming-metadata'
 import { decodeQueryPathParameter } from './lib/decode-query-path-parameter'
 import { NoFallbackError } from '../shared/lib/no-fallback-error.external'
 import { fixMojibake } from './lib/fix-mojibake'
@@ -271,11 +267,19 @@ export type RequestLifecycleOpts = {
   onAfterTaskError: ((error: unknown) => void) | undefined
 }
 
-type BaseRenderOpts = RenderOpts & {
+type ServerRenderConfig = Readonly<{
   poweredByHeader: boolean
   generateEtags: boolean
   previewProps: __ApiPreviewProps
-}
+  optimizeCss: NextConfigRuntime['experimental']['optimizeCss']
+  nextScriptWorkers: NextConfigRuntime['experimental']['nextScriptWorkers']
+  isExperimentalCompile: boolean | undefined
+}>
+
+type RequestRenderState = Readonly<{
+  params?: Params
+  error?: Error | null
+}>
 
 /**
  * The public interface for rendering with the server programmatically. This
@@ -304,7 +308,7 @@ export type RequestContext<
   res: ServerResponse
   pathname: string
   query: NextParsedUrlQuery
-  renderOpts: RenderOpts
+  renderState: RequestRenderState
 }
 
 // Internal wrapper around build errors at development
@@ -348,7 +352,7 @@ export default abstract class Server<
   protected readonly deploymentId: string
   protected readonly dev: boolean
   protected readonly minimalMode: boolean
-  protected readonly renderOpts: BaseRenderOpts
+  protected readonly serverRenderConfig: ServerRenderConfig
   protected readonly serverOptions: Readonly<ServerOptions>
   protected appPathRoutes?: Record<string, string[]>
   protected readonly clientReferenceManifest?: DeepReadonly<ClientReferenceManifest>
@@ -555,66 +559,13 @@ export default abstract class Server<
 
     this.nextFontManifest = this.getNextFontManifest()
 
-    this.renderOpts = {
-      dir: this.dir,
-      supportsDynamicResponse: true,
-      trailingSlash: this.nextConfig.trailingSlash,
+    this.serverRenderConfig = {
       poweredByHeader: this.nextConfig.poweredByHeader,
       generateEtags,
       previewProps: this.getPrerenderManifest().preview,
-      basePath: this.nextConfig.basePath,
-      images: this.nextConfig.images,
       optimizeCss: this.nextConfig.experimental.optimizeCss,
-      nextConfigOutput: this.nextConfig.output,
       nextScriptWorkers: this.nextConfig.experimental.nextScriptWorkers,
-      disableOptimizedLoading:
-        this.nextConfig.experimental.disableOptimizedLoading,
-      domainLocales: this.nextConfig.i18n?.domains,
-      distDir: this.distDir,
-      serverComponents: this.enabledDirectories.app,
-      cacheLifeProfiles: this.nextConfig.cacheLife,
-      staticPageGenerationTimeout: this.nextConfig.staticPageGenerationTimeout,
-      enableTainting: this.nextConfig.experimental.taint,
-      crossOrigin: this.nextConfig.crossOrigin
-        ? this.nextConfig.crossOrigin
-        : undefined,
-      largePageDataBytes: this.nextConfig.experimental.largePageDataBytes,
-
       isExperimentalCompile: this.nextConfig.experimental.isExperimentalCompile,
-      // `htmlLimitedBots` is passed to server as serialized config in string format
-      htmlLimitedBots: this.nextConfig.htmlLimitedBots,
-      cacheComponents: this.nextConfig.cacheComponents ?? false,
-      partialPrefetching: this.nextConfig.partialPrefetching,
-      validationLevel:
-        this.nextConfig.experimental.instantInsights.validationLevel,
-      experimental: {
-        expireTime: this.nextConfig.expireTime,
-        staleTimes: this.nextConfig.experimental.staleTimes,
-        clientTraceMetadata: this.nextConfig.experimental.clientTraceMetadata,
-        clientParamParsingOrigins:
-          this.nextConfig.experimental.clientParamParsingOrigins,
-        dynamicOnHover: this.nextConfig.experimental.dynamicOnHover ?? false,
-        optimisticRouting:
-          this.nextConfig.experimental.optimisticRouting ?? false,
-        inlineCss: this.nextConfig.experimental.inlineCss ?? false,
-        prefetchInlining:
-          this.nextConfig.experimental.prefetchInlining ?? false,
-        authInterrupts: !!this.nextConfig.experimental.authInterrupts,
-        useCacheTimeout: this.nextConfig.experimental.useCacheTimeout,
-        cachedNavigations:
-          this.nextConfig.experimental.cachedNavigations ?? false,
-        appShells: this.nextConfig.experimental.appShells,
-        maxPostponedStateSizeBytes: parseMaxPostponedStateSize(
-          this.nextConfig.experimental.maxPostponedStateSize
-        ),
-      },
-      onInstrumentationRequestError:
-        this.instrumentationOnRequestError.bind(this),
-      prefetchHints: {},
-      reactMaxHeadersLength: this.nextConfig.reactMaxHeadersLength,
-      logServerFunctions:
-        typeof this.nextConfig.logging === 'object' &&
-        Boolean(this.nextConfig.logging.serverFunctions),
     }
 
     this.pagesManifest = this.getPagesManifest()
@@ -1991,7 +1942,7 @@ export default abstract class Server<
     ) => Promise<ResponsePayload | null>,
     partialContext: Omit<
       RequestContext<ServerRequest, ServerResponse>,
-      'renderOpts'
+      'renderState'
     >
   ): Promise<void> {
     return getTracer().trace(BaseServerSpan.pipe, async () =>
@@ -2005,22 +1956,12 @@ export default abstract class Server<
     ) => Promise<ResponsePayload | null>,
     partialContext: Omit<
       RequestContext<ServerRequest, ServerResponse>,
-      'renderOpts'
+      'renderState'
     >
   ): Promise<void> {
-    const ua = partialContext.req.headers['user-agent'] || ''
-
     const ctx: RequestContext<ServerRequest, ServerResponse> = {
       ...partialContext,
-      renderOpts: {
-        ...this.renderOpts,
-        // `renderOpts.botType` is accumulated in `this.renderImpl()`
-        supportsDynamicResponse: !this.renderOpts.botType,
-        serveStreamingMetadata: shouldServeStreamingMetadata(
-          ua,
-          this.nextConfig.htmlLimitedBots
-        ),
-      },
+      renderState: {},
     }
 
     const payload = await fn(ctx)
@@ -2032,7 +1973,7 @@ export default abstract class Server<
     const { body } = payload
     let { cacheControl } = payload
     if (!res.sent) {
-      const { generateEtags, poweredByHeader } = this.renderOpts
+      const { generateEtags, poweredByHeader } = this.serverRenderConfig
 
       // In dev, we should not cache pages for any reason.
       if (this.dev) {
@@ -2060,15 +2001,12 @@ export default abstract class Server<
     ) => Promise<ResponsePayload | null>,
     partialContext: Omit<
       RequestContext<ServerRequest, ServerResponse>,
-      'renderOpts'
+      'renderState'
     >
   ): Promise<string | null> {
     const ctx: RequestContext<ServerRequest, ServerResponse> = {
       ...partialContext,
-      renderOpts: {
-        ...this.renderOpts,
-        supportsDynamicResponse: false,
-      },
+      renderState: {},
     }
     const payload = await fn(ctx)
     if (payload === null) {
@@ -2144,9 +2082,6 @@ export default abstract class Server<
       // (see custom-server integration tests)
       pathname = '/'
     }
-
-    const ua = req.headers['user-agent'] || ''
-    this.renderOpts.botType = getBotType(ua)
 
     // we allow custom servers to call render for all URLs
     // so check if we need to serve a static _next file or not.
@@ -2256,7 +2191,7 @@ export default abstract class Server<
       req,
       res,
       pathname,
-      renderOpts: opts,
+      renderState,
     }: RequestContext<ServerRequest, ServerResponse>,
     { components, query }: FindComponentsResult
   ): Promise<ResponsePayload | null> {
@@ -2530,28 +2465,6 @@ export default abstract class Server<
       }
     }
 
-    if (opts.supportsDynamicResponse === true) {
-      const ua = req.headers['user-agent'] || ''
-      const isBotRequest = isBot(ua)
-      const isSupportedDocument =
-        typeof components.Document?.getInitialProps !== 'function' ||
-        // The built-in `Document` component also supports dynamic HTML for concurrent mode.
-        NEXT_BUILTIN_DOCUMENT in components.Document
-
-      // Disable dynamic HTML in cases that we know it won't be generated,
-      // so that we can continue generating a cache key when possible.
-      // TODO-APP: should the first render for a dynamic app path
-      // be static so we can collect revalidate and populate the
-      // cache if there are no dynamic data requirements
-      opts.supportsDynamicResponse =
-        !isSSG && !isBotRequest && isSupportedDocument
-    }
-
-    // In development, we always want to generate dynamic HTML.
-    if (!isNextDataRequest && isAppPath && this.dev) {
-      opts.supportsDynamicResponse = true
-    }
-
     if (isSSG && this.minimalMode && req.headers[MATCHED_PATH_HEADER]) {
       // the url value is already correct when the matched-path header is set
       resolvedUrlPathname = urlPathname
@@ -2669,11 +2582,11 @@ export default abstract class Server<
     setRequestMeta(request, getRequestMeta(req))
     addRequestMeta(request, 'distDir', this.distDir)
     addRequestMeta(request, 'query', query)
-    addRequestMeta(request, 'params', opts.params)
+    addRequestMeta(request, 'params', renderState.params)
     addRequestMeta(request, 'minimalMode', this.minimalMode)
 
-    if (opts.err) {
-      addRequestMeta(request, 'invokeError', opts.err)
+    if (renderState.error) {
+      addRequestMeta(request, 'invokeError', renderState.error)
     }
 
     const maybeDevRequest: ServerRequest | IncomingMessage =
@@ -2772,7 +2685,7 @@ export default abstract class Server<
       locale: getRequestMeta(ctx.req, 'locale'),
       page,
       query,
-      params: ctx.renderOpts.params || {},
+      params: ctx.renderState.params || {},
       isAppPath,
       sriEnabled: !!this.nextConfig.experimental.sri?.algorithm,
       appPaths,
@@ -2868,8 +2781,8 @@ export default abstract class Server<
             {
               ...ctx,
               pathname: existingMatch.definition.pathname,
-              renderOpts: {
-                ...ctx.renderOpts,
+              renderState: {
+                ...ctx.renderState,
                 params: existingMatch.params,
               },
             },
@@ -3211,9 +3124,9 @@ export default abstract class Server<
           {
             ...ctx,
             pathname: statusPage,
-            renderOpts: {
-              ...ctx.renderOpts,
-              err,
+            renderState: {
+              ...ctx.renderState,
+              error: err,
             },
           },
           result
@@ -3247,11 +3160,11 @@ export default abstract class Server<
           {
             ...ctx,
             pathname: '/_error',
-            renderOpts: {
-              ...ctx.renderOpts,
+            renderState: {
+              ...ctx.renderState,
               // We render `renderToHtmlError` here because `err` is
               // already captured in the stacktrace.
-              err: isWrappedError
+              error: isWrappedError
                 ? renderToHtmlError.innerError
                 : renderToHtmlError,
             },
--- packages/next/src/server/dev/next-dev-server.ts
+++ packages/next/src/server/dev/next-dev-server.ts
@@ -122,6 +122,7 @@ export interface Options extends ServerOptions {
 export default class DevServer extends Server {
   // Override type to make the full config available instead of only NextConfigRuntime
   protected readonly nextConfig: NextConfigComplete
+  private readonly pagesErrorDebug = ReactDevOverlay
 
   /**
    * The promise that resolves when the server is ready. When this is unset
@@ -186,7 +187,6 @@ export default class DevServer extends Server {
     this.bundlerService = options.bundlerService
     this.startServerSpan =
       options.startServerSpan ?? trace('start-next-dev-server')
-    this.renderOpts.ErrorDebug = ReactDevOverlay
     this.staticPathsCache = new LRUCache(
       // 5MB
       5 * 1024 * 1024,
@@ -510,7 +510,7 @@ export default class DevServer extends Server {
     const span = trace('handle-request', undefined, { url: req.url })
     const result = await span.traceAsyncFn(async () => {
       await this.ready?.promise
-      addRequestMeta(req, 'PagesErrorDebug', this.renderOpts.ErrorDebug)
+      addRequestMeta(req, 'PagesErrorDebug', this.pagesErrorDebug)
       return await super.handleRequest(req, res, parsedUrl)
     })
     const memoryUsage = process.memoryUsage()
